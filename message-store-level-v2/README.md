9a88832eeaf8b6b11b4d8b76ebe5da55b2dd46f9
JakeFernandes98 <jakeneilaldo@hotmail.com>
2023-03-13 20:50:58 +0000

    removed message store dependency

--- message-store-level-v2/README.md
+++
@@ -1,7 +0,0 @@
- # Temporary
-
- This is a temporary repo that enables storage for the DWN using Level.
-
- # building
-
- `npm install && node bundle.js`
--- message-store-level-v2/src/index.js
+++
@@ -1,3 +0,0 @@
-export { Encoder } from './encoder.js';
-export { Blockstore } from './block-store.js';
-export { MessageStoreLevelv2 as MessageStore } from './message-store.js';
